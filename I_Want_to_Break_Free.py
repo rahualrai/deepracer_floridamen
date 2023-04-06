e3c4288d8e7fbaeb56c3092abde5cc30cbb02ae1
luminaa <rairahual7@gmail.com>
2023-04-06 13:42:37 -0400

    added reard function

---
+++ I_Want_to_Break_Free.py
@@ -0,0 +1,25 @@
+import math
+
+def reward_function(params):
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
+    
+    reward = 1.0
+    
+    next_point = waypoints[closest_waypoints[1]]
+    prev_point = waypoints[closest_waypoints[0]]
+    
+    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+    
+    track_direction = math.degrees(track_direction)
+    
+    direction_diff = abs(track_direction - heading)
+    
+    if direction_diff > 180:
+        direction_diff = 360 - direction_diff
+    
+    if direction_diff > 10.0:
+        reward *= 0.5
+    
+    return float(reward)
